dfd87b7c3c1d23c9307e8f1e726b3ff0321519eb
seb-the <sebastian.theunissen@gmail.com>
2025-09-28 18:45:33 +0200

    order changein main function

--- main.py
+++ main.py
@@ -19,11 +19,12 @@ def main():
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
-        pygame.Surface.fill(screen,(0,0,0))
+        dt = clock.tick(60) / 1000
         player.update(dt)
+        pygame.Surface.fill(screen,(0,0,0))
         player.draw(screen)
         pygame.display.flip()
-        dt = clock.tick(60) / 1000
+        
 
 
     print("Starting Asteroids!")
