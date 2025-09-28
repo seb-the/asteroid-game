9bc92e20afb1a2ba630083e563a24fee75c4cb5d
seb-the <sebastian.theunissen@gmail.com>
2025-09-28 18:28:38 +0200

    player can turn left and right

--- player.py
+++ player.py
@@ -2,7 +2,7 @@ import pygame
 
 from circleshape import CircleShape
 
-from constants import PLAYER_RADIUS
+from constants import PLAYER_RADIUS, PLAYER_TURN_SPEED
 
 class Player(CircleShape):
     def __init__(self, x, y):
@@ -20,3 +20,15 @@ class Player(CircleShape):
 
     def draw(self, screen):
         pygame.draw.polygon(screen, (255, 255, 255), self.triangle(), 2)
+
+    def rotate(self, dt):
+        self.rotation += (PLAYER_TURN_SPEED * dt)
+
+    def update(self, dt):
+        keys = pygame.key.get_pressed()
+
+        if keys[pygame.K_a]:
+            self.rotate(-dt)
+        if keys[pygame.K_d]:
+            self.rotate(dt)
+    
