109ba2617559ab7734c86ef4b83ac13ee7f88f66
seb-the <sebastian.theunissen@gmail.com>
2025-09-28 19:12:26 +0200

    player can move with a d w s

--- main.py
+++ main.py
@@ -19,7 +19,7 @@ def main():
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
-        dt = clock.tick(60) / 1000
+        dt = clock.tick(60) / 1000.0
         player.update(dt)
         pygame.Surface.fill(screen,(0,0,0))
         player.draw(screen)
