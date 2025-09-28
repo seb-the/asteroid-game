9b5d5efd62932f92d7f996bb66d4b814accc5488
seb-the <sebastian.theunissen@gmail.com>
2025-09-28 18:18:00 +0200

    add circleshape and player, spawn player in middle of screen

--- main.py
+++ main.py
@@ -5,16 +5,22 @@ import pygame
 
 from constants import *
 
+from circleshape import *
+
+from player import *
+
 def main():
     pygame.init
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
     clock = pygame.time.Clock()
     dt = 0
+    player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
     while True:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
         pygame.Surface.fill(screen,(0,0,0))
+        player.draw(screen)
         pygame.display.flip()
         dt = clock.tick(60) / 1000
 
