87cff96a321d364afc120ebbc396f93acfb3f2ad
seb-the <sebastian.theunissen@gmail.com>
2025-09-30 14:14:37 +0200

    creating groups and containers for players

--- main.py
+++ main.py
@@ -14,15 +14,22 @@ def main():
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
     clock = pygame.time.Clock()
     dt = 0
-    player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
+    # setting groups
+    updatable = pygame.sprite.Group()
+    drawable = pygame.sprite.Group()
+    # setting containers
+    Player.containers = (updatable, drawable)
+    # instantization
+    player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)    
     while True:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
         dt = clock.tick(60) / 1000.0
-        player.update(dt)
+        updatable.update(dt)
         pygame.Surface.fill(screen,(0,0,0))
-        player.draw(screen)
+        for thing in drawable:
+            thing.draw(screen)
         pygame.display.flip()
         
 
